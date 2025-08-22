f3d0576c84b4efccfe6028a29a6cf27e47de3eb2
Li Hongzhang <lihongzhang@bytedance.com>
2025-08-22 18:33:48 +0800

    init AttentionIsAllYouNeed

---
+++ dataset.py
@@ -0,0 +1,95 @@
+import torch
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import Dataset, DataLoader, random_split
+
+pad, sos, eos, seq_len = 0, 1, 2, 12
+
+
+class WordStat:
+    def __init__(self, path):
+        self.pairs, self.cmn_w2c, self.eng_w2c = [], {}, {}
+        with open(path, "r", encoding="utf-8") as f:
+            for line in f:
+                cmn, eng = line.lower().split("\t")
+                cmn, eng = cmn.strip().split(), eng.strip().split()
+                self.pairs.append((cmn, eng))
+                for word in cmn:
+                    self.cmn_w2c[word] = self.cmn_w2c.get(word, 0) + 1
+                for word in eng:
+                    self.eng_w2c[word] = self.eng_w2c.get(word, 0) + 1
+
+    def take(self, min_count=1, max_length=10):
+
+        cmn_w2c, eng_w2c = dict(self.cmn_w2c), dict(self.eng_w2c)
+
+        def predicate(cmn, eng):
+            cmn_map = map(lambda w: self.cmn_w2c[w] > min_count, cmn)
+            eng_map = map(lambda w: self.eng_w2c[w] > min_count, eng)
+            to_remove = (
+                max(len(cmn), len(eng)) > max_length
+                or not all(cmn_map)
+                or not all(eng_map)
+            )
+            if to_remove:
+                for word in cmn:
+                    cmn_w2c[word] -= 1
+                for word in eng:
+                    eng_w2c[word] -= 1
+            return not to_remove
+
+        def sorted_words(w2c):
+            sorted_item = sorted(w2c.items(), key=lambda wc: wc[1], reverse=True)
+            return [w for w, c in sorted_item if c]
+
+        cmn_eng = [pair for pair in self.pairs if predicate(*pair)]
+
+        return cmn_eng, sorted_words(cmn_w2c), sorted_words(eng_w2c)
+
+
+cmn_eng, cmn_words, eng_words = WordStat("./data/cmn-eng.txt").take(
+    min_count=5, max_length=seq_len - 3
+)
+
+
+class Dataset(Dataset):
+    def __init__(self, cmn_eng, cmn_words, eng_words):
+        self.data = []
+        for cmn, eng in cmn_eng:
+            x = [sos, *(cmn_words.index(w) + 3 for w in cmn), eos][1:-1]
+            y = [sos, *(eng_words.index(w) + 3 for w in eng), eos]
+            self.data.append((torch.tensor(x), torch.tensor(y)))
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __len__(self):
+        return len(self.data)
+
+
+def collate_fn(batch):
+    inputs = pad_sequence([x for x, _ in batch], batch_first=True, padding_value=pad)
+    outputs = pad_sequence([y for _, y in batch], batch_first=True, padding_value=pad)
+    return inputs, outputs
+
+
+def get_dataset(batch_size=32):
+    dataset = Dataset(cmn_eng, cmn_words, eng_words)
+    train_data, test_data = random_split(
+        dataset, [len(dataset) - batch_size, batch_size]
+    )
+    train_loader = DataLoader(
+        train_data, batch_size, shuffle=True, collate_fn=collate_fn
+    )
+    test_loader = DataLoader(test_data)
+
+    return train_loader, test_loader
+
+
+if __name__ == "__main__":
+    torch.manual_seed(9527)
+    print(len(cmn_eng), len(cmn_words) + 3, len(eng_words) + 3)
+    train_loader, test_loader = get_dataset()
+    for train, test in zip(*get_dataset()):
+        print(*map(lambda x: x.shape, train))
+        print(*test, sep="\n")
+        break
---
+++ model.py
@@ -0,0 +1,134 @@
+import math
+import torch
+import torch.nn as nn
+from functools import reduce
+
+model_size, hidden_size, n_layer, n_head, dropout = 512, 2048, 6, 8, 0.1
+
+
+class Transformer(nn.Module):
+    def __init__(self, input_size, output_size, *, max_len=1024, padding_idx=0):
+        super().__init__()
+        self.encoder, self.decoder = Encoder(), Decoder()
+
+        self.emi = nn.Embedding(input_size, model_size, padding_idx)
+        self.emo = nn.Embedding(output_size, model_size, padding_idx)
+        self.lin = nn.Linear(model_size, output_size)
+        self.pos = PositionalEncoding(max_len)
+        self.out = nn.Dropout(dropout)
+        self.pad = padding_idx
+
+    def forward(self, inputs, outputs):
+        x, mx, y, my = *self.feature(inputs), *self.feature(outputs)
+        return self.decoder(self.encoder(x, mx), mx, y, my)
+
+    def feature(self, indices):
+        return self.out(self.emi(indices) + self.pos(indices)), indices == self.pad
+
+
+class Encoder(nn.ModuleList):
+    def __init__(self):
+        super().__init__(EncoderBlock() for _ in range(n_layer))
+
+    def forward(self, x, mx):
+        return reduce(lambda x, f: f(x, mx), self, x)
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.attn = AddAndNorm(MultiHeadAttention())
+        self.feed = AddAndNorm(FeedForward())
+
+    def forward(self, x, mx):
+        m = mx.unsqueeze(-2) | mx.unsqueeze(-1)
+        x = self.attn(x, x, x, m=m)
+        x = self.feed(x)
+        return x
+
+
+class Decoder(nn.ModuleList):
+    def __init__(self):
+        super().__init__(DecoderBlock() for _ in range(n_layer))
+
+    def forward(self, x, mx, y, my):
+        return reduce(lambda y, f: f(x, mx, y, my), self, y)
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.att1 = AddAndNorm(MultiHeadAttention(casual=True))
+        self.att2 = AddAndNorm(MultiHeadAttention())
+        self.feed = AddAndNorm(FeedForward())
+
+    def forward(self, x, mx, y, my):
+        m1 = my.unsqueeze(-2) | my.unsqueeze(-1)
+        m2 = mx.unsqueeze(-2) | my.unsqueeze(-1)
+        y = self.att1(y, y, y, m=m1)
+        y = self.att2(y, x, x, m=m2)
+        y = self.feed(y)
+        return y
+
+
+class AddAndNorm(nn.Module):
+    def __init__(self, sub_layer):
+        super().__init__()
+        self.norm = nn.LayerNorm(model_size)
+        self.out = nn.Dropout(dropout)
+        self.sub = sub_layer
+
+    def forward(self, x, *args, **kargs):
+        y = self.out(self.sub(x, *args, **kargs))
+        return self.norm(x + y)
+
+
+class MultiHeadAttention(nn.ModuleList):
+    def __init__(self, casual=False):
+        super().__init__(nn.Linear(model_size, model_size) for _ in range(3))
+        self.lin, self.casual = nn.Linear(model_size, model_size), casual
+
+    def forward(self, *qkv, m):
+        q, k, v = (
+            x.view(*x.shape[:-1], n_head, -1).transpose(-3, -2)
+            for x in (f(x) for f, x in zip(self, qkv))
+        )
+        w = q @ k.mT / math.sqrt(q.size(-1))
+        m = m | (~m).triu(1) if self.casual else m
+        w = w.masked_fill(m.unsqueeze(-3), float("-inf"))
+        v = (w.softmax(dim=-1).nan_to_num(0) @ v).transpose(-3, -2)
+        v = v.reshape(*v.shape[:-2], -1)
+        return self.lin(v)
+
+
+class FeedForward(nn.Sequential):
+    def __init__(self):
+        super().__init__(
+            nn.Linear(model_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, model_size),
+        )
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, max_len):
+        super().__init__()
+        pos = torch.arange(max_len, dtype=torch.float).unsqueeze(-1)
+        emb = torch.arange(0, model_size, 2, dtype=torch.float)
+        div = (-math.log(10000.0) / model_size * emb).exp()
+        pe = torch.zeros(max_len, model_size)
+        pe[:, 0::2] = torch.sin(pos * div)
+        pe[:, 1::2] = torch.cos(pos * div)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        return self.pe[: x.size(-1)]
+
+
+if __name__ == "__main__":
+    input_size, output_size, batch_size = 3234, 2313, 32
+    model = Transformer(input_size, output_size)
+    inputs = torch.randint(input_size, (batch_size, 10))
+    outputs = torch.randint(output_size, (batch_size, 20))
+    assert model(inputs, outputs).argmax(dim=-1).shape == outputs.shape
+    print(model)
